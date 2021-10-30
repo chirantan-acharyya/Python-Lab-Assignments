5d1ad492f4c2b83ee23ce82d7dff6c0a30f67ccb
i-am-chirantan <acharyya.chirantan@gmail.com>
2021-10-30 18:30:26 +0530

    File Modified

--- Assignment3/Prog 7.py
+++ Assignment3/Prog 7.py
@@ -8,9 +8,8 @@ s1 = 0
 s2 = 0
 l1 = list(map(int, input().split()))
 for i in range(len(l1)):
-    if i & 2 == 1:
+    if i & 1 == 1:
         s1 = s1 + l1[i]     # odd index
-
     else:
         s2 = s2 + l1[i]     # even index
 print(s2 - s1)
